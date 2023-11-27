10292dd3c85b94ad3813c9ff206469e4239ad1ad
Void <skeletonvoid0@gmail.com>
2023-11-27 11:01:56 +0800

    - Bumped version to v0.1.5.3 - Added setting useOfflineUUIDForPlayerData   Does what the name suggests. This setting is used for Vastech to migrate from no forwarding to BungeeCord forwarding and give player skins.

--- carpetmodSrc/carpet/CarpetSettings.java
+++ carpetmodSrc/carpet/CarpetSettings.java
@@ -45,7 +45,7 @@ public class CarpetSettings
     public static boolean locked = false;
 
     // TODO: replace these constants at build time
-    public static final String tagVersion = "v0.1.5.2";
+    public static final String tagVersion = "v0.1.5.3";
     public static final String carpetVersion = "Carpet Vastech Addition " + tagVersion;
 
     public static final String minecraftVersion = "1.12.2";
@@ -1298,6 +1298,9 @@ public class CarpetSettings
 
     @Rule(desc = "Fixes the crash loops related to ArrayIndexOutOfBounds when trying to freezing dragons", category = FIX)
     public static boolean dragonFreezingCrashFix = false;
+
+    @Rule(desc = "Online player use offline UUID for player data", category = FEATURE)
+    public static boolean useOfflineUUIDForPlayerData = false;
 
 
     // ===== API ===== //
